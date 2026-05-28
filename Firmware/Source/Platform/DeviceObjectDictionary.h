1731ba026b6394e0444533e6e27b6b5986cf815b
asytyi <alexander.a.sytyi@gmail.com>
2026-05-28 12:12:21 +0300

    Выпилен device ID

--- Firmware/Source/Platform/DeviceObjectDictionary.h
+++ Firmware/Source/Platform/DeviceObjectDictionary.h
@@ -101,8 +101,7 @@
 // -----------------------------
 #define REG_FWINFO_SLAVE_NID					256	// Device CAN slave node ID
 #define REG_FWINFO_MASTER_NID					257	// Device CAN master node ID (if presented)
-#define REG_DEVICE_ID							258	// Device ID
-// 259
+
 #define REG_FWINFO_STR_LEN						260	// Length of the information string record
 #define REG_FWINFO_STR_BEGIN					261	// Begining of the information string record
 
